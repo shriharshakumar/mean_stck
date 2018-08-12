5562f2e7e0698e4890070653539a7afb5a8fd1e1
shriharshakumar <shriharshakumar@gmail.com>
2018-08-12 22:29:46 +0530

    Added back button.

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   console.log(err);
-  res.render('error', { message: err.message , error: {} });
+  res.send('error', { message: err.message , error: {} });
 });
 
 module.exports = app;
